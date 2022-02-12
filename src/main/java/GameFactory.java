41c5fc57332092cf21236b1b1cba23438388df3e
EgorEko <asya.wear@gmail.com>
2022-02-12 19:04:14 +0200

    Add GUI implementation

--- src/main/java/GameFactory.java
+++ src/main/java/GameFactory.java
@@ -2,6 +2,7 @@ import component.*;
 import component.console.ConsoleDataPrinter;
 import component.console.ConsoleGameOverHandler;
 import component.console.ConsoleUserInputReader;
+import component.swing.GameWindow;
 
 /**
  * @author devonline
@@ -14,9 +15,20 @@ public class GameFactory {
 
     public Game create() {
         final NumberGenerator numberGenerator = new NumberGenerator();
-        final DataPrinter dataPrinter = new ConsoleDataPrinter();
-        final UserInputReader userInputReader = new ConsoleUserInputReader(dataPrinter);
-        final GameOverHandler gameOverHandler = new ConsoleGameOverHandler();
+        final DataPrinter dataPrinter;
+        final UserInputReader userInputReader;
+        final GameOverHandler gameOverHandler;
+        //FIXME: Must be GUI mode
+        if (1 == 1) {
+            final GameWindow gameWindow = new GameWindow();
+            dataPrinter = gameWindow;
+            userInputReader = gameWindow;
+            gameOverHandler = gameWindow;
+        } else {
+            dataPrinter = new ConsoleDataPrinter();
+            userInputReader = new ConsoleUserInputReader(dataPrinter);
+            gameOverHandler = new ConsoleGameOverHandler();
+        }
         return new Game(numberGenerator, dataPrinter, userInputReader, gameOverHandler);
     }
 }
