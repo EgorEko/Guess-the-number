38cc2170a080b18545138cd4d576e9c8a32b05d3
EgorEko <asya.wear@gmail.com>
2022-02-12 09:38:11 +0200

    Create new game configuration

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,12 @@
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public final class Launcher {
+
+    public static void main(final String[] args) {
+        final GameFactory gameFactory = new GameFactory(args);
+        final Game game = gameFactory.create();
+        game.play();
+    }
+}
